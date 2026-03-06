8d30043149debbefd013cfc59a5609839290a7f2
sneurlax <sneurlax@gmail.com>
2026-03-05 22:17:58 -0600

    refactor: rename example app to nix_example and add workspace resolution

--- apps/example/test/widget_test.dart
+++ apps/example/test/widget_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:nix/main.dart';
+import 'package:nix_example/main.dart';
 
 void main() {
   testWidgets('app renders', (tester) async {
